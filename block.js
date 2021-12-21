cf9086e906b527df9812ee34e302f77c14553278
Jarrad-Harvey <jarrad@harves.net>
2021-12-21 14:03:46 +0800

    newItem creates a dynamic item instead of a basic list

--- block.js
+++ block.js
@@ -1,23 +1,31 @@
+var emptyItem, list;
+
 function updateBlockList(){
     chrome.storage.sync.set({key: value}, function() {
         console.log('Value is set to ' + value);
     });
 }
 
-function addSite(){
-    var text = document.getElementById("site").value;
-    var node = document.createElement("Li");
-    node.innerHTML = text;
-    document.getElementById("list").appendChild(node);
+function addItem(){
+    let newItem = document.getElementById("newItem").value;
+    let e = emptyItem.cloneNode(true);
+    e.getElementsByClassName("btn-danger")[0].addEventListener("click",function(){deleteItem(this)});
+    e.firstElementChild.firstElementChild.innerHTML = newItem;
+    e.style.visibility = "visible";
+    list.appendChild(e);
 }
 
-function deleteSite(e){
+function deleteItem(e){
     e.parentNode.parentNode.remove();
 }
 
 window.onload = function(e){
+    list = document.getElementById("list");
+    emptyItem = list.lastElementChild.cloneNode(true);
+    list.lastElementChild.remove();
+
     // event listeners
-    document.getElementById('add').addEventListener('click',addSite);
-    buttons = document.getElementsByClassName("btn-danger");
-    for(var i=0; i<buttons.length; i++) buttons[i].addEventListener('click',function(){deleteSite(this)});
+    document.getElementById('add').addEventListener('click',addItem);
+    // buttons = document.getElementsByClassName("btn-danger");
+    // for(var i=0; i<buttons.length; i++) buttons[i].addEventListener('click',function(){deleteItem(this)});
 }
